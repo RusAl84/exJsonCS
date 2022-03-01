ebd1f78e86b1162c1e1c6eb5aae082883c1c0f3f
Aleksey <rusal@bk.ru>
2022-03-02 00:32:38 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exJsonCS
+{
+    [Serializable]
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public bool Sex { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using exJsonCS;
+using Newtonsoft.Json;
+
+//1
+string fileJsonString = File.ReadAllText("1.json");
+Person tmpPerson = new Person();
+tmpPerson = JsonConvert.DeserializeObject<Person>(fileJsonString);
+string sexString = "";
+if (tmpPerson.Sex == true)
+    sexString = "Male";
+else
+    sexString = "Female";
+Console.WriteLine($"Имя: {tmpPerson.Name} Возраст: {tmpPerson.Age} Пол: {sexString}");
+
+
+//2
+Person bred = new Person();
+bred.Name = "Pit";
+bred.Age = 49;
+string jsonString = JsonConvert.SerializeObject(bred, Formatting.Indented);
+File.WriteAllText("bred.json", jsonString);
+Console.WriteLine("Файл записан");
+
+
+//3
+string tmpString = File.ReadAllText("bred.json");
+Console.WriteLine(tmpString);
